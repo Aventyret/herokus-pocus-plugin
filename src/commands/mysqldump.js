79014891092a549c992dff1c47fb24dc54958d2b
Ivar Bergman <ivarbergman@gmail.com>
2017-07-11 15:00:23 +0200

    Fixed lint errors.

--- src/commands/mysqldump.js
+++ src/commands/mysqldump.js
@@ -1,31 +1,23 @@
-// @flow
-import {Command, flags} from 'cli-engine-heroku'
-import url from 'url';
-import credentials from '../credentials';
-import { spawn, execSync } from 'child_process';
+import { Command, flags } from 'cli-engine-heroku'
+import credentials from '../credentials'
+import { execSync } from 'child_process'
 
-export default class HelloWorld extends Command {
+export default class MysqlDumpCommand extends Command {
   static topic = 'pocus'
   static command = 'mysqldump'
   static description = 'mysqldump client connected to a heroku app database.'
   static flags = {
-    app: flags.app({required: true}),
+    app: flags.app({required: true})
   }
   static args = [
     {name: 'tables', optional: true}
   ]
 
   async run () {
-
     const config = await this.heroku.get(`/apps/${this.flags.app}/config-vars`)
-
     let cred = credentials(config)
-
     let tables = this.args.tables || ''
-
     let cmd = `mysqldump --single-transaction -u ${cred.user} -p${cred.pwd} -h ${cred.host} ${cred.database} ${tables}`
-
-    execSync(cmd, {stdio: 'inherit'});
-
+    execSync(cmd, {stdio: 'inherit'})
   }
 }
--- src/credentials.js
+++ src/credentials.js
@@ -1,25 +1,18 @@
-// @flow
-import url from 'url';
+
+import url from 'url'
 
 export default (config) => {
+  let dburl = ''
+  if (config.hasOwnProperty('DATABASE_URL')) {
+    dburl = config.DATABASE_URL
+  } else if (config.hasOwnProperty('JAWSDB_URL')) {
+    dburl = config.JAWSDB_URL
+  } else if (config.hasOwnProperty('CLEARDB_URL')) {
+    dburl = config.CLEARDB_URL
+  }
+  let u = url.parse(dburl)
+  let [user, pwd] = u.auth.split(':', 2)
+  let path = u.path.replace(/\//, '')
 
-    let dburl = ''
-    if (config.hasOwnProperty('DATABASE_URL'))
-    {
-      dburl = config.DATABASE_URL
-    }
-    else if (config.hasOwnProperty('JAWSDB_URL'))
-    {
-      dburl = config.JAWSDB_URL
-    }
-    else if (config.hasOwnProperty('CLEARDB_URL'))
-    {
-      dburl = config.CLEARDB_URL
-    }
-    let u = url.parse(dburl)
-    let [user, pwd] = u.auth.split(':',2)
-    let path = u.path.replace(/\//, "")
-
-    return {user, pwd, host: u.hostname, database: path}
-
+  return { user, pwd, host: u.hostname, database: path }
 }
